4c7dcb51d315d438e14505da8026485327d6bece
alialkaisar92 <alialkaisar002@gmail.com>
2026-07-27 02:18:16 +0300

    Add Google Tag Manager codes

--- server.js
+++ server.js
@@ -172,15 +172,17 @@ app.post('/api/support', (req, res) => {
   res.json({ message: 'تم إرسال رسالتك للدعم ✓' });
 });
 
-// مسار صريح لإثبات ملكية جوجل عبر ملف HTML[span_1](start_span)[span_1](end_span)
-app.get('/googleb92b2cd0a1a64ca9.html', (req, res) => {
-  res.send('google-site-verification: googleb92b2cd0a1a64ca9.html');
-});
-
 app.get('/', (req, res) => {
   res.send(`<!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MFTGNDMX');</script>
+<!-- End Google Tag Manager -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 <title>صفقة استور - Safqa Store | منصة التسويق بالعمولة</title>
@@ -284,6 +286,10 @@ textarea{min-height:90px;resize:vertical}
 </style>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFTGNDMX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header class="header">
   <div class="logo" id="ht">Safqa Store</div>
   <button class="cart-btn" onclick="go('cart')">🛒 السلة<span class="badge" id="cc">0</span></button>
